fd7b6b36fb80ff08b7c5840d91506ad3f5a6a0d7
MieszkoBu <115088598+MieszkoBu@users.noreply.github.com>
2022-10-11 22:06:38 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+def dzielenie(dzielna, dzielnik):
+    if(dzielnik == 0):
+        return 
+    else:
+        return dzielna / dzielnik
+
+print(dzielenie(8, 4))
+print(dzielenie(20, 0))
+
